903c6e8082e108a33930d68005ad5e996631ab65
Micha Alt <mail@lean-stack.de>
2016-03-12 13:29:38 +0100

    Using UMD pattern from now on.

--- src/core.js
+++ src/core.js
@@ -1,4 +1,20 @@
-(function(global) {
+// UMD pattern: https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.lean = factory();
+  }
+}(this, function () {
+
   'use strict';
 
   var lean = {
@@ -6,6 +22,5 @@
     lesson:  'Part 1: The Very Beginning.'
   };
 
-  global.lean = lean;
-
-}(window));
+  return lean;
+}));
